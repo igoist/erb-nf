83ea91783326ac7368544c63a198123ff9578721
igoist <493512132@qq.com>
2019-07-24 10:31:45 +0800

    AudioPlayer preparation

--- script/link.sh
+++ script/link.sh
@@ -8,3 +8,9 @@ ln -s "$(pwd)/public/img/" "$(pwd)/dist/img"
 
 echo "link $(pwd)/public/font -> $(pwd)/dist/font"
 ln -s "$(pwd)/public/font/" "$(pwd)/dist/font"
+
+echo "link $(pwd)/public/map -> $(pwd)/dist/map"
+ln -s "$(pwd)/public/map/" "$(pwd)/dist/map"
+
+echo "link $(pwd)/public/music -> $(pwd)/dist/music"
+ln -s "$(pwd)/public/music/" "$(pwd)/dist/music"
