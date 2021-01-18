9e9a3ae4e26e6f39fbe451fc5455bf0815e54e50
igoist <493512132@qq.com>
2021-01-18 23:34:44 +0800

    feature/tabs-admin: tabs with activeKeyIndex & support change tab with keybindings

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,29 +1,66 @@
 import * as React from 'react';
+import { Tabs } from 'antd';
+import { useKeyPress } from 'ahooks';
+
 import T from './T';
 import T2 from './T2';
-import { Tabs } from 'antd';
-
 import TabLinkItem from './TabLinkItem';
 import TabLinkItemType from './TabLinkItemType';
 
 const { TabPane } = Tabs;
 
 const A = () => {
+  const tabsArr = [
+    {
+      name: '栏目自定义',
+      component: <T />,
+    },
+    {
+      name: 'Tab 2',
+      component: <T2 />,
+    },
+    {
+      name: 'TabLinkItem',
+      component: <TabLinkItem />,
+    },
+    {
+      name: 'TabLinkItemType',
+      component: <TabLinkItemType />,
+    },
+  ];
+
+  const tabsArrLength = tabsArr.length;
+
+  const [activeKeyIndex, setActiveKeyIndex] = React.useState(3);
+
+  useKeyPress(['meta.shift.[', 'meta.['], () => {
+    setActiveKeyIndex((activeKeyIndex + tabsArrLength - 1) % 4);
+  });
+
+  useKeyPress(['meta.shift.]', 'meta.]'], () => {
+    setActiveKeyIndex((activeKeyIndex + tabsArrLength + 1) % 4);
+  });
+
+  const handleOnChange = (activeKey: any) => {
+    console.log(activeKey);
+
+    setActiveKeyIndex(parseInt(activeKey.slice(4, activeKey.length)));
+  };
+
+  const TabPanes = React.useMemo(
+    () =>
+      tabsArr.map((tab, index) => (
+        <TabPane tab={tab.name} key={`tab-${index}`}>
+          {tab.component}
+        </TabPane>
+      )),
+    []
+  );
+
   return (
     <div style={{ padding: '20px', height: '560px', overflow: 'scroll' }}>
-      <Tabs defaultActiveKey='1'>
-        <TabPane tab='栏目自定义' key='1'>
-          <T />
-        </TabPane>
-        <TabPane tab='Tab 2' key='2'>
-          <T2 />
-        </TabPane>
-        <TabPane tab='TabLinkItem' key='TabLinkItem'>
-          <TabLinkItem />
-        </TabPane>
-        <TabPane tab='TabLinkItemType' key='TabLinkItemType'>
-          <TabLinkItemType />
-        </TabPane>
+      <Tabs activeKey={`tab-${activeKeyIndex}`} onChange={handleOnChange}>
+        {TabPanes}
       </Tabs>
     </div>
   );
